c955bc47801f2de66dc2dad27597540c187a55a2
khanhpdph05706 <khanhpdph05706@fpt.edu.vn>
2018-10-31 07:44:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/khanhpd/khanhpdph05706_ass/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.khanhpd.khanhpdph05706_ass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
